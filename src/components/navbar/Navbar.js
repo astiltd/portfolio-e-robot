90653013dc0e173ceae66de82ac834054a5f5f57
astiltd <astiltd@github.com>
2022-04-15 15:07:01 +0600

    initial navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='nav-container'>
+          <h1>EveryDay <br/> Robot</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
